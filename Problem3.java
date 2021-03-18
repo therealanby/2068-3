daf3fa32e89e5b04ae92fe53576d70b3331f3bfa
Andy Chen <therealanby@gmail.com>
2021-03-18 21:10:25 +0000

    Initial commit

---
+++ Problem3.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Problem3 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static PrintWriter pw = new PrintWriter(System.out,true);
+
+    static StringTokenizer st;
+
+    public static void main(String[] args){
+        try{
+            run();
+            pw.close();
+            br.close();
+        }catch(IOException IOE){
+            pw.println("Error with I/O");
+        }catch(NumberFormatException NFE){
+            pw.println("Invalid Input");
+        }catch(Exception E){
+            pw.println("Something went wrong.");
+        }
+    }
+
+    public static void run() throws IOException{
+
+    }
+    public static void solve(){
+
+    }
+}
